025bc12ae159f170b9392a748a19cfc35fdf38d6
Priscila Schleske <plasch7@gmail.com>
2023-08-21 15:53:14 -0600

    solves all steps of lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,67 @@
+import playerData from "./playerData";
+import { useState } from "react";
+
 function BaseballCard(props) {
-  return (
-    <div className="card">
-      <h2>Player Name goes here</h2>
-    </div>
-  );
+  const [showPicture, setShowPicture] = useState(true);
+  function toggleCard() {
+    setShowPicture(!showPicture);
+  };
+
+  if (showPicture) {
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <img src={props.imgUrl} alt={props.name} />
+      </div>
+    );
+  } else {
+    const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+      <p key={statName}>
+        {statName}: {statValue}
+      </p>
+    ));
+
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{props.name}</h2>
+        <p>Team: {props.team}</p>
+        <p>Position: {props.position}</p>
+        {statsDisplay}
+      </div>
+    );
+  }
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => <BaseballCard
+    name={player.name}
+    team={player.team}
+    position={player.position}
+    stats={player.stats}
+    imgUrl={player.imgUrl}
+    key={player.cardId} />
+  );
+
+  return <>
+    {cards}
+  </>;
 }
 
 export default App;
+
+// destructuring const cards
+// const cards = playerData.map(({ name, team, position, stats, imgUrl, cardId }) => (
+// <BaseballCard 
+// name={name}
+// team={team}
+// position={position}
+// stats={stats}
+// imgUrl={imgUrl}
+// key={cardId}
+// />
+// ));
+
+// this works too:
+// const cards = playerData.map((player) => (
+// <BaseballCard {...player} key={player.cardId />}
+// ))
